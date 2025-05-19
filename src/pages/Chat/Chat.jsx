2386001bdf25133b20227111f74a480bb2d5435b
astikadarsh <astikadarsh7061@gmail.com>
2025-05-19 23:27:08 +0530

    completed video tutorial till 51 mins

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
 const Chat= () => {
   return (
-    <div>
-        
+    <div className='chat'>
+        <div className="chat-container">
+            <LeftSidebar />
+            <ChatBox/>
+            <RightSidebar/>
+
+        </div>
     </div>
   )
 }
