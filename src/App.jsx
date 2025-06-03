405efbc3a1da1017fddb0b3bfcc3e4f219ead3c5
astikadarsh <astikadarsh7061@gmail.com>
2025-06-03 13:47:51 +0530

    completed video till 2 hour 39 min

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
@@ -6,29 +6,30 @@ import Chat from './pages/Chat/Chat'
 import { ToastContainer, toast } from 'react-toastify';
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './config/firebase'
-const App= () => {
-  const navigate=useNavigate();
-
-useEffect(()=>{
-  onAuthStateChanged(auth,async (user) => {
-    if (user) {
-      navigate('/chat')
-      console.log(user);
-    }
-    else{
-      navigate('/')
-    }
-  })
-},[])
+import { AppContext } from './context/AppContext'
+const App = () => {
+  const navigate = useNavigate();
+  const { loadUserData } = useContext(AppContext)
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate('/chat')
+        await loadUserData(user.uid)
+      }
+      else {
+        navigate('/')
+      }
+    })
+  }, [])
 
   return (
     <>
-    <ToastContainer/>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/chat' element={<Chat/>}/>
-      <Route path='/profile' element={<ProfileUpdate/>}/>
-    </Routes>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/profile' element={<ProfileUpdate />} />
+      </Routes>
     </>
   )
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,14 +1,26 @@
+import { doc, getDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
-
+import { db } from "../config/firebase";
 export const AppContext=createContext();
 const AppContextProvider=(props)=>{
 
     const [userData,setUserData]=useState(null);
     const [chatData,setChatData]=useState(null);
 
+    const loadUserData=async (uid) => {
+        try {
+            const userRef= doc(db,'users',uid);
+            const userSnap=await getDoc(userRef);
+            console.log(userSnap);
+        } catch (error) {
+            
+        }
+    }
+
     const value={
         userData,setUserData,
-        chatData,setChatData
+        chatData,setChatData,
+        loadUserData
     }
     return(
         <AppContext.Provider value={value}> 
